5799bd03113c9968a62d3c22a2b03371eebf5d91
leo-g7 <leo@whyyle.com>
2022-05-09 00:33:18 +0200

    Lexical Analysis

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <windows.h>
 #include <stdio.h>
 #include "services/buffer/buffer.h"
+#include "services/lexer/lexer.h"
 
 int main(int argc, char* argv[])
 {
@@ -16,6 +17,30 @@ int main(int argc, char* argv[])
       if (file != NULL)
       {
         buf_init(&buffer, file);
+
+         char *getalphanum = lexer_getalphanum(&buffer);
+        printf("%s\n",getalphanum);
+
+        buf_print(&buffer);
+
+        if (getalphanum != NULL)
+        {
+          free(getalphanum);
+        }
+
+        char *getalphanum_rollback = lexer_getalphanum_rollback(&buffer);
+
+        buf_print(&buffer);
+
+        if (getalphanum_rollback != NULL)
+        {
+          free(getalphanum_rollback);
+        }
+
+        long getnumber = lexer_getnumber(&buffer);
+
+        printf("%ld\n",getnumber);
+
       }
       else
       {
