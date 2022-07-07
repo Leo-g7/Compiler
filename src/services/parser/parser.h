4d55845ca74fa132eeb74eb5cb44b39855bc03c9
leo-g7 <leo@whyyle.com>
2022-07-08 01:03:57 +0200

    Analyze function content

--- src/services/parser/parser.h
+++ src/services/parser/parser.h
@@ -7,6 +7,13 @@
 bool is_expected_lexem (char *lexem, char *expected);
 int get_variable_type(char *lexem);
 int analyze_type(buffer_t * buffer);
+void analyze_return(buffer_t * buffer);
+void analyze_condition(buffer_t * buffer);
+void analyze_loop(buffer_t * buffer);
+void analyze_init(buffer_t * buffer);
+void analyze_assignation(buffer_t * buffer);
+void analyze_instruction(buffer_t * buffer,char* lexem);
+void analyze_function_body(buffer_t * buffer);
 ast_list_t* analyze_function_params(buffer_t * buffer);
 void analyze_function(buffer_t * buffer);
 void parser(FILE *file);
