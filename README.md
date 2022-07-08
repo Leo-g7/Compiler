64f2d0c64c49e39054ae7a626ce95601ccde9fc3
leo-g7 <leo@whyyle.com>
2022-07-08 23:27:12 +0200

    comments

--- README.md
+++ README.md
@@ -11,13 +11,8 @@ gcc src/main.c src/services/buffer/buffer.c src/utils/utils.c src/services/lexer
 ```
 pws
 .\app.exe input/test.intech
-bash 
+bash
 ./app.exe input/test.intech
 ```
 
 ## Note
-
-loop with while
-and do a if case
-check if we re into bracket so we make sure to be inside a function
-else search for a function
--- src/services/symbol/symbol.c
+++ src/services/symbol/symbol.c
@@ -3,59 +3,85 @@
 #include <stdlib.h>
 #include <string.h>
 
-symbol_t *sym_new(char *name, int type, ast_t *attributes) {
+// Initialize the symbol table
+symbol_t *sym_new(char *name, int type, ast_t *attributes)
+{
+    // Allocate memory for the symbol
     symbol_t *node = (symbol_t *)malloc(sizeof(symbol_t));
+    // Set the name, type, and attributes
     node->name = name;
     node->type = type;
     node->attributes = attributes;
     return node;
 }
 
-void sym_delete(symbol_t *sym) {
+// Delete the symbol table
+void sym_delete(symbol_t *sym)
+{
+    // Free the symbol
     free(sym);
 }
-
-void sym_remove(symbol_t **table, symbol_t *sym) {
+// Remove the symbol from the table
+void sym_remove(symbol_t **table, symbol_t *sym)
+{
     symbol_t *tmp = *table;
-    if (tmp == sym) {
+    // If the symbol is the first one, set the table to the second one
+    if (tmp == sym)
+    {
         *table = tmp->next;
         return;
     }
-
-    while (tmp->next != NULL) {
-        if (tmp->next == sym) {
+    // Otherwise, find the symbol and remove it
+    while (tmp->next != NULL)
+    {
+        // If the next symbol is the one to remove, remove it
+        if (tmp->next == sym)
+        {
             tmp->next = tmp->next->next;
             return;
         }
         tmp = tmp->next;
     }
 }
-
-void sym_add(symbol_t **table, symbol_t *sym) {
+// Add the symbol to the table
+void sym_add(symbol_t **table, symbol_t *sym)
+{
     symbol_t *tmp = *table;
-    if (tmp == NULL) {
+    // If the table is empty, set the first symbol
+    if (tmp == NULL)
+    {
         *table = sym;
         return;
     }
-    while (tmp->next != NULL) {
+    // Otherwise, find the last symbol and add it
+    while (tmp->next != NULL)
+    {
         tmp = tmp->next;
     }
     tmp->next = sym;
 }
-
-symbol_t *sym_search(symbol_t *table, char *name) {
+// Find the symbol in the table
+symbol_t *sym_search(symbol_t *table, char *name)
+{
     symbol_t *tmp = table;
-    while (tmp != NULL) {
-        if (strcmp(tmp->name, name) == 0) {
+    while (tmp != NULL)
+    {
+        // If the name matches, return the symbol
+        if (strcmp(tmp->name, name) == 0)
+        {
             return tmp;
         }
         tmp = tmp->next;
     }
     return NULL;
 }
-void sym_print_list(symbol_t *table) {
+// Print the symbol table
+void sym_print_list(symbol_t *table)
+{
     symbol_t *tmp = table;
-    while (tmp != NULL) {
+    while (tmp != NULL)
+    {
+        // Print the symbol
         printf("%s\n", tmp->name);
         tmp = tmp->next;
     }
